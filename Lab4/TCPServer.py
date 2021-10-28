7990637effc9309a39b640f9c4cab84dbd2c381e
Marc Friedenberg <maf946@psu.edu>
2021-10-28 18:02:53 -0400

    Update TCPServer.py
    Random server port

--- Lab4/TCPServer.py
+++ Lab4/TCPServer.py
@@ -6,7 +6,7 @@ def get_ip_address():
 	return s.getsockname()[0]
 	
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.bind(('', 12000))
+serverSocket.bind(('', 0))
 
 serverIP = get_ip_address()
 serverPort = serverSocket.getsockname()[1]
