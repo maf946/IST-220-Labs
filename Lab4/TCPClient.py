d055513c12845bb0fb06756430cc3d018bc47575
Marc Friedenberg <maf946@psu.edu>
2024-03-14 14:43:52 -0400

    Error handling

--- Lab4/TCPClient.py
+++ Lab4/TCPClient.py
@@ -1,16 +1,21 @@
 import socket
 
-serverIP = "127.0.0.1" #replace this number
-serverPort = 5000 #replace this number
+serverIP = "104.39.31.51"  # replace this number; keep the quotes
+serverPort = 64998  # replace this number; do not add quotes
 
-print("I'm configured to send TCP packets to " + serverIP + " on port " + str(serverPort))
-print ("Press Ctrl+Z to quit.")
-
-while 1:
-	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	clientSocket.connect((serverIP, serverPort))
-	message = input("Input lowercase text: ")
-	clientSocket.send(message.encode())
-	modifiedMessage = clientSocket.recv(1024)
-	print("Returned from server: " + modifiedMessage.decode())
-	clientSocket.close()
+while True:
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.settimeout(5)
+            client_socket.connect((serverIP, serverPort))
+            message = input("Input lowercase text (type 'quit' to exit): ")
+            if message.lower() == 'quit':
+                break
+            try:
+                client_socket.send(message.encode())
+                modified_message = client_socket.recv(1024).decode()
+                print("Returned from server:", modified_message)
+            except socket.timeout:
+                print("Request timed out")
+    except socket.error as e:
+        print("Error:", e)
--- Lab4/TCPServer.py
+++ Lab4/TCPServer.py
@@ -1,29 +1,35 @@
 import socket
 
+
 def get_ip_address():
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.connect(("8.8.8.8", 80))
-	return s.getsockname()[0]
-	
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.bind(('', 12000))
+serverSocket.bind(('', 0))
 
 serverIP = get_ip_address()
 serverPort = serverSocket.getsockname()[1]
 
 serverSocket.listen(1)
 
-print ("serverIP: " + serverIP)
-print ("serverPort:     " + str(serverPort))
-print ("Press Ctrl+Z to quit. Listening...")
+print("serverIP: " + serverIP)
+print("serverPort:     " + str(serverPort))
+print("Listening...")
 
-while 1:
-	connectionSocket, addr = serverSocket.accept()
-	clientIP = str(addr[0])
-	clientPort = str(addr[1])
-	message = connectionSocket.recv(1024).decode()
-	if message:
-		print ("Received from " + clientIP + "#" + clientPort + ": " + message)
-		modifiedMessage = message.upper()
-		connectionSocket.send(modifiedMessage.encode())
-		connectionSocket.close()
+while True:
+    try:
+        connectionSocket, addr = serverSocket.accept()
+    except Exception as e:
+        print("Error accepting connection:", e)
+
+    clientIP = str(addr[0])
+    clientPort = str(addr[1])
+    message = connectionSocket.recv(1024).decode()
+    if message:
+        print("Received from " + clientIP + "#" + clientPort + ": " + message)
+        modifiedMessage = message.upper()
+        connectionSocket.send(modifiedMessage.encode())
+        connectionSocket.close()
