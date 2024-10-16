f0ec27a0cf303666bd6129b02d5c4f5e862d7967
Marc Friedenberg <maf946@psu.edu>
2024-10-16 08:31:23 -0400

    Update UDPClient.py

--- Lab4/UDPClient.py
+++ Lab4/UDPClient.py
@@ -1,14 +1,14 @@
 import socket
 
-serverIP = "127.0.0.1" #replace this number
-serverPort = 5000 #replace this number
+serverIP = "192.168.0.19" #replace this number
+serverPort = 54667 #replace this number
 
 print("I'm configured to send UDP packets to " + serverIP + " on port " + str(serverPort))
-print ("Press Ctrl+Z to quit.")
+print ("Press Ctrl+C to quit.")
 
 while 1:	
 	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)		
-	message = input("Input lowercase text: ")
+	message = input("Input text to scramble: ")
 	clientSocket.sendto(message.encode(), (serverIP, serverPort))
 	modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 	print ("Returned from server: " + modifiedMessage.decode())
